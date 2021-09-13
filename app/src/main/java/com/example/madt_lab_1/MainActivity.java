937cf0dd21b5bae44ca20532908f244c6751c512
alicebrykun <fkbcfm@gmail.com>
2021-09-13 17:36:21 +0300

    Added button that changes color

--- app/src/main/java/com/example/madt_lab_1/MainActivity.java
+++ app/src/main/java/com/example/madt_lab_1/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.madt_lab_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.tvMain = findViewById(R.id.tvMainLabel);
+
+        Button btnChangeText = (Button) findViewById(R.id.btnChangeText);
+        Button btnChangeColor = (Button) findViewById(R.id.btnChangeColor);
     }
 
     public void onBtnChangeTextClick (View view) {
         this.tvMain.setText("The 1st changes! Hooray!!!");
+    }
+
+    public void onBtnChangeColorClick (View view) {
+        this.tvMain.setTextColor(Color.rgb(255,192,0));
     }
 }
