3bb9c30c971f4b4e739c845dd9c8cf23e5eabb7f
alicebrykun <fkbcfm@gmail.com>
2021-09-13 17:05:07 +0300

    Added button that changes text

--- app/src/main/java/com/example/madt_lab_1/MainActivity.java
+++ app/src/main/java/com/example/madt_lab_1/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.madt_lab_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMainLabel);
+    }
+
+    public void btnChangeClick (View view) {
+        this.tvMain.setText("The 1st changes! Hooray!!!");
     }
 }
